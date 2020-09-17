c2c0d748c686e652f5fc453c6bbe78dd79d9cac2
Y R <y.ryzhkoff@gmail.com>
2020-09-17 10:48:36 -0400

    Add SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
@@ -34,12 +35,9 @@ class App extends Component {
         <Route 
           exact
           path="/palette/:paletteId/:colorId" 
-          render={() => <h1>Single color page!</h1>} 
+          render={() => <SingleColorPalette />} 
         />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[3])} />
-      // </div>
     );
   }
 }
