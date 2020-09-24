94f138e12523b5b4a6728de2357d9017980efe01
Y R <y.ryzhkoff@gmail.com>
2020-09-24 14:23:28 -0400

    Add NewPaletteForm

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteListStyles";
@@ -13,7 +14,8 @@ class PaletteList extends Component {
             <div className={classes.root}>
                 <div className={classes.container}>
                     <nav className={classes.nav}>
-                        <h1>React Colors</h1>
+                        <h1>React Colors App</h1>
+                        <Link to="/palette/new">Create Palette</Link>
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
